b2b62fbbcbef9fd328282d273144e632771494c3
HX <im@bgzo.cc>
2025-01-01 00:14:37 +0800

    feat: main support param & fix octal error.

--- main.sh
+++ main.sh
@@ -9,31 +9,28 @@ get_date_from_string() {
     date -d "$1" +"%Y-%m-%d"
 }
 
-# 计算生日到今天的天数
 days_since_birthday() {
     birthday=$(date -d "$1" +"%s")
-    today=$(date +"%s")
-    echo $(( (today - birthday) / 86400 ))
+    target_day=$(date -d "$2" +"%s")
+    echo $(( (target_day - birthday) / 86400 ))
 }
 
-# 计算生日到今天的周数
 weeks_since_birthday() {
-    days=$(days_since_birthday "$1")
+    days=$(days_since_birthday "$1" "$2")
     echo $(( days / 7 ))
 }
 
-# 计算生日到今天的月数
 months_since_birthday() {
     birthday=$(date -d "$1" +"%Y-%m-%d")
-    today=$(date +"%Y-%m-%d")
+    target_day=$(date -d "$2" +"%Y-%m-%d")
 
     birth_year=$(date -d "$birthday" +"%Y")
-    birth_month=$(date -d "$birthday" +"%m")
-    birth_day=$(date -d "$birthday" +"%d")
+    birth_month=$((10#$(date -d "$birthday" +"%m")))
+    birth_day=$((10#$(date -d "$birthday" +"%d")))
 
-    current_year=$(date +"%Y")
-    current_month=$(date +"%m")
-    current_day=$(date +"%d")
+    current_year=$(date -d "$target_day" +"%Y")
+    current_month=$((10#$(date -d "$target_day" +"%m")))
+    current_day=$((10#$(date -d "$target_day" +"%d")))
 
     years_difference=$(( current_year - birth_year ))
     months_difference=$(( (years_difference * 12) + (current_month - birth_month) ))
@@ -47,15 +44,15 @@ months_since_birthday() {
 # 计算年龄（年和月）
 age_in_years_and_months() {
     birthday=$(date -d "$1" +"%Y-%m-%d")
-    today=$(date +"%Y-%m-%d")
+    target_day=$(date -d "$2" +"%Y-%m-%d")
 
     birth_year=$(date -d "$birthday" +"%Y")
-    birth_month=$(date -d "$birthday" +"%m")
-    birth_day=$(date -d "$birthday" +"%d")
+    birth_month=$((10#$(date -d "$birthday" +"%m")))
+    birth_day=$((10#$(date -d "$birthday" +"%d")))
 
-    current_year=$(date +"%Y")
-    current_month=$(date +"%m")
-    current_day=$(date +"%d")
+    current_year=$(date -d "$target_day" +"%Y")
+    current_month=$((10#$(date -d "$target_day" +"%m")))
+    current_day=$((10#$(date -d "$target_day" +"%d")))
 
     age_years=$(( current_year - birth_year ))
     age_months=$(( current_month - birth_month ))
@@ -85,15 +82,18 @@ write_colored_line() {
     esac
 
     reset_color="\033[0m"
-    printf "${color_code}| %-10s %-30s |\n" "$label" "$value"
+    printf "${color_code}| %-10s %-28s |\n" "$label" "$value"
 }
+
+# 接收参数，默认为今天
+target_date=${1:-$(date +"%Y-%m-%d")}
 
 # 生日
 my_birthday="2001-08-07"
-days_passed=$(days_since_birthday "$my_birthday")
-weeks_passed=$(weeks_since_birthday "$my_birthday")
-months_passed=$(months_since_birthday "$my_birthday")
-age=$(age_in_years_and_months "$my_birthday")
+days_passed=$(days_since_birthday "$my_birthday" "$target_date")
+weeks_passed=$(weeks_since_birthday "$my_birthday" "$target_date")
+months_passed=$(months_since_birthday "$my_birthday" "$target_date")
+age=$(age_in_years_and_months "$my_birthday" "$target_date")
 
 # 输出进度框
 echo -e "\033[1;36m+-----------------------------------------+"
